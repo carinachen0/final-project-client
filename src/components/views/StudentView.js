a97feba3964d52e51d262443aae459084cb94da7
Carina Chen <carina.chen66@myhunter.cuny.edu>
2025-12-10 23:45:12 -0500

    added a clickable button link to edit student

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,6 +4,8 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import { Link } from "react-router-dom";
+
 const StudentView = (props) => {
   const { student } = props;
 
@@ -19,6 +21,9 @@ const StudentView = (props) => {
       ) : (
         <h3>Campus: Currently not enrolled</h3>
       )}
+      <Link to={`/editStudent/${student.id}`}>
+        <button>Edit Student</button>
+      </Link>
     </div>
   );
 
